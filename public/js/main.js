ef99ff3f358d672c29645ef20907a6522fb260f8
Anthony Gonzalez <anthonywgonz@gmail.com>
2021-04-05 14:24:42 -0400

    added all files

---
+++ public/js/main.js
@@ -0,0 +1,72 @@
+const todoComplete = document.querySelectorAll('.completedTick')
+const todoUnComplete = document.querySelectorAll('.completed')
+const deleted = document.querySelectorAll('.fas')
+
+Array.from(todoComplete).forEach((element) =>{
+    element.addEventListener('click', markDone)
+
+})
+
+Array.from(todoUnComplete).forEach((element) =>{
+    element.addEventListener('click', unComplete)
+})
+
+Array.from(deleted).forEach((element) =>{
+    element.addEventListener('click', deleteItem)
+})
+
+async function markDone(){
+    const todoInput = this.parentNode.childNodes[1].innerText
+    console.log(todoInput)
+    try{
+        const res = await fetch('markDone', {
+            method:'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              'todoInput': todoInput
+            })
+          })
+        const data = await res.json()
+            console.log(data)
+            
+        }catch(err){
+        console.log(err)
+    }
+}
+
+
+async function unComplete(){
+    const todoInput = this.parentNode.childNodes[1].innerText
+    try{
+        const res = await fetch('UnComplete', {
+            method: 'put',
+            headers: {'Content-Type': 'applications/json'},
+            body: JSON.stringify({
+                'todoInput' : todoInput
+            })
+           })
+        const data = await res.json()
+        console.log(data)
+   
+    }catch(err){
+        console.log(err)
+    }
+
+}
+
+async function deleteItem(){
+    const todoInput = this.parentNode.childNodes[1].innerText
+    try{
+        const res = await fetch('deleteItem', {
+            method: 'delete',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'todoInput': todoInput
+            })
+        })
+        const data =  await res.json()
+        console.log(data)
+    }catch(err){
+        console.log(err)
+    }
+}
---
+++ server.js
@@ -0,0 +1,85 @@
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+const PORT = 3000
+require('dotenv').config()
+
+
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'todoDb'
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    })
+
+    app.set('view engine', 'ejs')
+    app.use(express.static('public'))
+    app.use(express.urlencoded({ extended: true }))
+    app.use(express.json())
+
+    app.listen(process.env.PORT || PORT, ()=>{
+        console.log(`this server is running on port ${PORT}`);
+    })  
+
+
+    app.get('/',async (req, res)=>{
+        const todoItems = await db.collection('todos').find().toArray()
+        res.render('index.ejs', { todoItem: todoItems})
+        })
+
+        app.post('/addTodo', (req, res) => {
+            db.collection('todos').insertOne({todoInput: req.body.todoInput, completed: false})
+            .then(res => {
+                console.log('Todo Added')
+            })
+            .catch(error => console.error(error))
+        })
+
+
+        app.put('/markDone', (req, res) => {
+            db.collection('todos').updateOne({todoInput: req.body.todoInput},{
+                $set: {
+                    completed: true
+                  }
+                },{
+                    upsert: false    
+            })
+            .then(result => {
+                console.log('Marked as done')
+                res.json('Marked as done')
+            })
+            .catch(error => console.error(error))
+        })
+  
+
+
+        app.put('/unComplete', (req, res) => {
+            db.collection('todos').updateOne({todoInput: req.body.todoInput},{
+                $set: {
+                    completed: false
+                  }
+            },{
+                upsert: false
+            })
+            .then(result => {
+                console.log('Marked UnComplete')
+                res.json('Marked Complete')
+            })
+            .catch(error => console.error(error))
+        
+        })
+
+        app.delete('/deleteItem', (req, res) => {
+            db.collection('todos').deleteOne({todoInput: request.body.todoInput})
+            .then(result => {
+                console.log('Todo Deleed')
+                res.json('Todo Deleted')
+            })
+            .catch(error => console.log(error))
+        })
+
+
+   
